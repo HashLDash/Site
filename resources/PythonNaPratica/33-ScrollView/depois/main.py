74c1e0ee6587316901308ca133982e2029a88488
William Scaff <williamscaff.usp@gmail.com>
2017-11-09 06:34:36 -0200

    Adicionado video 33 ScrollView

---
+++ resources/PythonNaPratica/33-ScrollView/depois/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+
+class Tarefas(ScrollView):
+    def __init__(self,tarefas,**kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.ids.box.add_widget(Label(text=tarefa,font_size=30,size_hint_y=None,height=200))
+
+class Test(App):
+    def build(self):
+        return Tarefas(['Fazer compras','Buscar filho','Molhar a calçada','iu','iuhy','iug'])
+
+Test().run()
